158244aed4ac5d64e365c9de95ef8128630b4ae9
Sabuhi <sabuhies-ap118@code.edu.az>
2024-11-21 00:03:28 +0400

    First commit gonna chack if it is working when someone dowloads it

---
+++ Uniqlo0.1/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uniqlo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Admin()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Uniqlo0.1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Uniqlo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+    }
+}

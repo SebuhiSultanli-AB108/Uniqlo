d165a1cdbd3169776692eca7e9836e4c7704a6dd
Sabuhi <sabuhies-ap118@code.edu.az>
2024-11-21 00:15:50 +0400

    ???

--- Uniqlo0.1/obj/Debug/net8.0/Uniqlo0.1.AssemblyInfo.cs
+++ Uniqlo0.1/obj/Debug/net8.0/Uniqlo0.1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uniqlo0.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3782297ee283fa98d1ae0ee5344524f44cdb8ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uniqlo0.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uniqlo0.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
